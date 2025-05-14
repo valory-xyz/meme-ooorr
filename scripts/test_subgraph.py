0790a22d25667ac4e50cb1bfb3824d31ac67ebce
David Vilela <dvilelaf@gmail.com>
2025-05-14 11:24:49 +0200

    fix: revert change

--- scripts/test_subgraph.py
+++ scripts/test_subgraph.py
@@ -196,7 +196,7 @@ def introspect_subgraph():
 
 # introspect_subgraph()
 
-meme_coin_data = get_memeooorr_handles_from_subgraph()
+meme_coin_data = get_meme_coins_from_subgraph()
 
 # Print the meme coin data in a formatted JSON output
 print(json.dumps(meme_coin_data, indent=4))
