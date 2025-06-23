5d52672c973459249ce88b1e2ebd046d9ffe3e91
Axat <akshat@solulab.co>
2025-06-23 15:55:51 +0530

    Revert "Feature/chat UI persona handler"

--- packages/dvilela/skills/memeooorr_abci/models.py
+++ packages/dvilela/skills/memeooorr_abci/models.py
@@ -20,7 +20,7 @@
 """This module contains the shared state for the abci skill of MemeooorrAbciApp."""
 
 from dataclasses import dataclass
-from typing import Any, Callable, Dict, Optional, Tuple
+from typing import Any, Dict, Optional
 
 from aea.skills.base import SkillContext
 
@@ -52,8 +52,6 @@ class SharedState(BaseSharedState):
         super().__init__(*args, skill_context=skill_context, **kwargs)
         self.twitter_username: Optional[str] = None
         self.twitter_id: Optional[str] = None
-
-        self.req_to_callback: Dict[str, Tuple[Callable, Dict[str, Any]]] = {}
 
 
 Requests = BaseRequests
